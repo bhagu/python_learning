8c7d9f5fc72088fd21837185777ba83b100fbec4
Bhagyaraj Chitoth <Bhagyarajc@gmail.com>
2017-10-30 17:32:17 +0530

    test

---
+++ hello.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 16:58:48 2017
+
+@author: bhagu
+"""
+
+import sys
+
+def main():
+    print('Hello World!!!')
+    print(len(sys.argv))
+    
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+    main()
